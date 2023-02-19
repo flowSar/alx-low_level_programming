ba2c1298e53eb36e26ebe0e9d663ae9fda7c85ff
flowsar <brahim.sar151@gmail.com>
2023-02-19 11:32:43 +0100

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *Return always 1 (failed)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	/* return 1 means that there is some error while */
+	return (1);
+}
