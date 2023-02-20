3afcc95b85b4bd6e008c283933d5e296285c8102
flowsar <brahim.sar151@gmail.com>
2023-02-20 23:15:30 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int nm [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'e', 'f'};
 
-	for (int i = 0;  i < 15; i++)
+	for (int i = 0;  i <= 15; i++)
 	{
 		putchar(nm[i]);
 	}
