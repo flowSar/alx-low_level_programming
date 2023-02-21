17e77d29d47d278d12420936db712bc2dae54d7a
flowsar <brahim.sar151@gmail.com>
2023-02-21 22:33:23 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,22 +1,2 @@
-#include "main.h"
+#include <stdio.h>
 
-void print_alphabet(void)
-{
-    char c = 'a';
-    while (c <= 'z')
-    {
-        _putchar(c);
-        ++c;
-    }
-    _putchar('\n');
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
