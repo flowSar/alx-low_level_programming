433b44721aa80556687f0f187b21023cb707510b
flowsar <brahim.sar151@gmail.com>
2023-02-21 21:47:56 +0100

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * print_alphabet - pritn phabet in lowercase
@@ -10,10 +23,10 @@ void print_alphabet(void)
 	char c = 'a';
 	while (c <= 'z')
 	{
-		putchar(c);
+		_putchar(c);
 		++c;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
 
 /**
