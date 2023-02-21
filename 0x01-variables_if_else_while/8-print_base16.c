6dace729110cd097f3abb05d891dab456bb9baa3
flowsar <brahim.sar151@gmail.com>
2023-02-21 18:50:38 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char n [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'e', 'f'};
+
+	for (int i = 0; i <= 15; i++)
+	{
+		putchar(n[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
