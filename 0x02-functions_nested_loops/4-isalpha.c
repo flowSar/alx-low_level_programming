b4e1acb2ff252892367e80ee1996895bb3e8ddb9
flowsar <brahim.sar151@gmail.com>
2023-02-22 18:56:07 +0100

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * _isalpha - isalpha
+ * @c: input
+ *
+ * Description: checks if a letter is alpha
+ * Return: Always 1 is input is alpha
+ */
+int _isalpha(int c)
+{
+	if (islower(c) || isupper(c))
+		return (1);
+	else
+		return (0)
+}
