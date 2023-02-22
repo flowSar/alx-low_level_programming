00f774563439e2d6aca1a6dceeb097ba4522b47a
flowsar <brahim.sar151@gmail.com>
2023-02-22 17:47:52 +0100

    . I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,27 +5,6 @@
 
 
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-void print_alphabet(void)
-{
-    char c = 'a';
-    while (c <= 'z')
-    {
-        _putchar(c);
-        ++c;
-    }
-    _putchar('\n');
-}
 
 /**
  * main - check the code for Holberton School students.
