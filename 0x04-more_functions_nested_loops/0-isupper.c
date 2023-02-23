6f15afa1d155f3ce1a44d15e99d962c3f52b68b3
flowsar <brahim.sar151@gmail.com>
2023-02-23 23:40:10 +0100

    isUpper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
 #include <ctype.h>
 #include "main.h"
 /**
-* _isupper - isupper funvtion
-* @c: input
-* 
-* Description: check if c is uper or ot
-* Return: 1 if upper and 0 of not
+*_isupper - isupper funvtion
+*@c: input
+*Description: check if c is uper or ot
+*Return: 1 if upper and 0 of not
 */
 int _isupper(int c)
 {
-	if (isupper(c))
-		return (1);
-	return (o);
+if (isupper(c))
+	return (1);
+return (o);
 }
