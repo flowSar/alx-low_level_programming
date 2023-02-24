33d34d09f0678594a108809c4f270026b44536b4
flowsar <brahim.sar151@gmail.com>
2023-02-24 19:59:56 +0100

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,16 +3,16 @@
 /**
 *_isdigit - isdigitr function
 *@c: input
-*Description: check if c is digit or not 
+*Description: check if c is digit or not
 *Return: 1 if c is dit  and 0 if not
 */
 int _isdigit(int c)
 {
-	if (c <= 9 && c >= 0)
-	{
-		if (isalpha(c))
-			return (1);
-		return (0);
-	}
+if (c <= 9 && c >= 0)
+{
+	if (isalpha(c))
+		return (1);
 	return (0);
+}
+return (0);
 }
