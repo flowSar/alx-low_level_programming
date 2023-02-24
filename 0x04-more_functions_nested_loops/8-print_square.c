b13b2f9111335a77346d9e257b575d5e8207c8b2
flowsar <brahim.sar151@gmail.com>
2023-02-24 23:14:49 +0100

    ou are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - this function print rectangle of # depends on arg
+ * @size: input
+ *
+ * Return: always 0
+ */
+void print_triangle(int size)
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		int j = 0;
+
+		while (j < size)
+		{
+			_putchar('#');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
