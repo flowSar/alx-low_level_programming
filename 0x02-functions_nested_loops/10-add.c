d6d45bece2d4dd3b9b7269bba32bfe5ce0cc3a70
flowsar <brahim.sar151@gmail.com>
2023-02-26 15:29:13 +0100

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add- add to number and return the result
+ * @a: value
+ * @b: value
+ *
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
