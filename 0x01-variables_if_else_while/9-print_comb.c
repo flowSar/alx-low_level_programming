32a64273e9cccbe5544157ad9efe95aa41d0df01
flowsar <brahim.sar151@gmail.com>
2023-02-26 18:01:37 +0100

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,9 @@
 int main(void)
 {
 	char x [] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int i;
 
-	for (int i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(x[i]);
 		putchar(',');
