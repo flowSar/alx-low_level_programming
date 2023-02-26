07e302396c53b2816df04d76337556db9b6fd31c
flowsar <brahim.sar151@gmail.com>
2023-02-26 12:26:11 +0100

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,23 +4,24 @@
  *
  * Return: always 0 (success)
  */
- int main(void)
- {
- 	int i;
- 	for (i = 1; i <= 100; i++)
- 	{
- 		if (i % 3 == 0)
- 		{
- 			printf("Fizz");
- 		}
- 		else if (i % 5 == 0)
- 		{
- 			printf("Buzz");
- 		}
- 		else
- 		{
- 			printf(" %d ", i);
- 		}
- 	}
- 	return (0);
- }
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf(" %d ", i);
+		}
+	}
+	return (0);
+}
