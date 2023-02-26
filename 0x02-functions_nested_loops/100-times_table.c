6efafe1d41ad5bd97cb62aa921431e18e6ab921a
flowsar <brahim.sar151@gmail.com>
2023-02-26 15:52:27 +0100

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,8 +28,8 @@ void print_times_table(int n)
 				}
 			}else if (res >= 100)
 			{
-				_putchar(res / 10 + '0');
 				_putchar(res / 100 + '0');
+				_putchar((res / 10 ) % 10 + '0');
 				_putchar(res % 10 + '0');
 			}
 			else
