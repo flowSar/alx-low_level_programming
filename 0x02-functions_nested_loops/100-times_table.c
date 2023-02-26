618207837e897c082fd1d89baa667966da49f1a0
flowsar <brahim.sar151@gmail.com>
2023-02-26 17:25:17 +0100

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,54 +7,48 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
-
-	for (i = 0; i <= n; i++)
+	if (n < 15 & n >0 )
 	{
-		for (j = 0; j <= n; j++)
-		{
-			int res = i * j;
+		int i, j;
 
-			if (res < 10)
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
 			{
-				if (j != 0)
+				int res = i * j;
+
+				if (res < 10)
+				{
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(res % 10 + '0');
+					if (j != n)
+						_putchar(',');
+				}
+				else if (res >= 100)
+				{
+					_putchar(' ');
+					_putchar(res / 100 + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar(res % 10 + '0');
+					if (j != n)
+						_putchar(',');
+				}
+				else
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+					if (j != n)
+						_putchar(',');
 				}
-				_putchar(res % 10 + '0');
-				if (j != n)
-					_putchar(',');
 			}
-			else if (res >= 100)
-			{
-				_putchar(' ');
-				_putchar(res / 100 + '0');
-				_putchar((res / 10) % 10 + '0');
-				_putchar(res % 10 + '0');
-				if (j != n)
-					_putchar(',');
-			}
-			else if (res >= 1000)
-			{
-				_putchar(res / 1000 + '0');
-				_putchar((res / 100) % 10 + '0');
-				_putchar((res / 10) % 10 + '0');
-				_putchar(res % 10 + '0');
-				if (j != n)
-					_putchar(',');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(res / 10 + '0');
-				_putchar(res % 10 + '0');
-				if (j != n)
-					_putchar(',');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
