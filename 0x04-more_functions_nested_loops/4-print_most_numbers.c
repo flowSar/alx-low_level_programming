1c722462f55c4006051a240d305ea6d186d16c7f
flowsar <brahim.sar151@gmail.com>
2023-02-26 10:50:20 +0100

    print_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
-
-	while (i <= 9)
-	{
-		if (i == 2 || i == 4)
-			continue;
-		else
-			_putchar(i);
-		i++;
-	}
-	_putchar('\n');
+        char nm[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        int i = 0;
+        while (i < 9)
+        {
+                _putchar(nm[i]);
+                i++;
+        }
+        _putchar('\n');
 }
