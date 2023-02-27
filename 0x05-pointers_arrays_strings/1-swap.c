e8e6e600f296b838945878398a47746b54d835a0
flowsar <brahim.sar151@gmail.com>
2023-02-27 22:12:26 +0100

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function change  the value of that pointer point to
+ * @n: input
+ *
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+    
+}
