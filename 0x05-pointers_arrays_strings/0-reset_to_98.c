c9523547606d082fcf5bd542d066f5852a26608e
flowsar <brahim.sar151@gmail.com>
2023-02-27 22:00:37 +0100

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ * reset_to_98 - function change  the value of that pointer point to
+ * @n: input
+ *
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 89;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this directory has most taks about pointer and arrays
