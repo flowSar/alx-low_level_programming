8034d9a6d69b0de314d310506f91b6c8b3ff02e4
flowsar <brahim.sar151@gmail.com>
2023-02-27 22:48:21 +0100

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - sget length of a tring
+ * @s : input
+ *
+ * Return: the length ofstring
+ */
+int _strlen(char *s)
+{
+	len = strlen(s);
+	return (len);
+}
