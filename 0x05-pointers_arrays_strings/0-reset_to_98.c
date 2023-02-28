534e40cfea81d53566157b18bd5f1cb8bfe6d479
flowsar <brahim.sar151@gmail.com>
2023-02-28 23:13:46 +0100

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -10,3 +10,15 @@ void reset_to_98(int *n)
 	*n = 89;
 }
 
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    n = 402;
+    printf("%d\n", n);
+    reset_to_98(&n);
+    printf("%d\n", n);
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - print half of a string
+ * @dest : input
+ * @src: input
+ *
+ * Return: the value of dest
+ */
+int _atoi(char *s)
+{
+    
+}
