1c52fc4cdcded4cad38f0993e25f95fdad113aa6
flowsar <brahim.sar151@gmail.com>
2023-02-28 19:00:06 +0100

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - sget length of a tring
+ * @str : input
+ *
+ * Return: the length ofstring
+ */
+void puts2(char *str)
+{
+	int i, l;
+
+	l = strlen(str);
+	for (i = 0 ; i > ; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
