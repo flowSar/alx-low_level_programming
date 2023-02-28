69692db45529c942a89729bd6c60d1fdad16b6b6
flowsar <brahim.sar151@gmail.com>
2023-02-28 21:56:00 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - print half of a string
+ * @dest : input
+ * @src: input
+ *
+ * Return: the value of dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, l;
+
+	l = strlen(src);
+	for (i = 0; i < l; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
