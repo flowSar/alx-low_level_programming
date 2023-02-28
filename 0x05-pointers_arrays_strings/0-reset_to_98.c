69b57ee4b936d067be41b6a45ebf89d40e77e0cb
flowsar <brahim.sar151@gmail.com>
2023-02-28 23:14:25 +0100

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,16 +9,3 @@ void reset_to_98(int *n)
 {
 	*n = 89;
 }
-
-#include <stdio.h>
-
-int main(void)
-{
-    int n;
-
-    n = 402;
-    printf("%d\n", n);
-    reset_to_98(&n);
-    printf("%d\n", n);
-    return (0);
-}
