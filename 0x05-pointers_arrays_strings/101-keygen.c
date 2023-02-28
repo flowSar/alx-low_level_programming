edf8c08fab38a4b0f3c195f4e628e57f1f9c0c6c
flowsar <brahim.sar151@gmail.com>
2023-02-28 23:30:00 +0100

    reat leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _atoi - sget length of a tring
+ * @s : input
+ *
+ * Return: the length ofstring
+ */
+int _atoi(char *s)
+{
+	atoi(s);
+
+	return (0);
+}
