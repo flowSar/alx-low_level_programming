1f43e1fac10d1cbb2efc9101c13bf8bd573b52d5
flowsar <brahim.sar151@gmail.com>
2023-03-02 21:23:02 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - sget length of a tring
+ * @s : input
+ *
+ * Return: the length ofstring
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return ('0');
+}
