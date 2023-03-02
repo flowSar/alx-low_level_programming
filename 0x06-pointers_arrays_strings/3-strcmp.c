aa56185e955a53d178162c9216032d65bad89b0a
flowsar <brahim.sar151@gmail.com>
2023-03-02 22:58:25 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,6 +11,16 @@ int _strcmp(char *s1, char *s2)
 {
 	int result;
 
-	strcmp(s1, s2);
+	result = strcmp(s1, s2);
 	return (result);
+}
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
