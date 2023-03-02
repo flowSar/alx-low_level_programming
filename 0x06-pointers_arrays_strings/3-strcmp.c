4282bb5d6236370ffa5e7fa6b4d7ad662234ddff
flowsar <brahim.sar151@gmail.com>
2023-03-02 22:54:00 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - sget length of a tring
+ * @s1 : input
+ * @s2 : input
+ *
+ * Return: 0 if success
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+    strcmp(s1, s2);
+
+    return (0);
+}
