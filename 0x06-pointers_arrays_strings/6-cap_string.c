7ff41f9cff0f59893a9f6791de0f8e098da7a02f
flowsar <brahim.sar151@gmail.com>
2023-03-03 22:45:56 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,13 +1,32 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 /**
- * cap_string - sget length of a tring
+ * cap_string - A function that capitalizes all words of a string.
+ * @str: string input
  *
- * Return: 0 if success
+ * Return: string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	char *p;
+	char *result;
+	int pred, i;
 
-	return (p);
+	result = str;
+	pred = 1;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (pred && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		pred = 0;
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			pred = 1;
+		}
+	}
+	return (result);
 }
