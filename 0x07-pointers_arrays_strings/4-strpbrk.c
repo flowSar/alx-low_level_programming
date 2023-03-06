c3228759a48f273ebdb8016799f7c0a82455bf6a
flowsar <brahim.sar151@gmail.com>
2023-03-06 23:41:53 +0100

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ * @s : input
+ * @accept : input
+ *
+ * Return: return the rest of pointer,
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+}
