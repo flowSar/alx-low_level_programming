0db64afe6c18424aaa45f4f08ec8303416199d1f
flowsar <brahim.sar151@gmail.com>
2023-03-07 00:12:45 +0100

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,21 +3,20 @@
 #include <stdio.h>
 /**
  * print_diagsums - function that searches a string for any of a set of bytes
- * @s : input
+ * @a : input
  * @size : input
  *
  * Return: return the rest of pointer,
  */
 void print_diagsums(int *a, int size)
 {
-	int sum = 0,sum2 = 0, i, j;
+	int sum = 0, sum2 = 0, i, j;
 
-	for (i = 0; i < size*size; i+=size+1)
+	for (i = 0; i < size * size; i += size + 1)
 	{
 		sum += a[i];
 	}
-	
-	for (j = size-1; j < (size*size)-size+1; j+=size-1)
+	for (j = size - 1; j < (size * size) - size + 1; j += size - 1)
 	{
 		sum2 += a[j];
 	}
