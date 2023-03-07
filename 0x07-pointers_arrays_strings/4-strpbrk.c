201756ee82a9b0565faf7d3a47f83016d6737000
flowsar <brahim.sar151@gmail.com>
2023-03-07 18:39:32 +0100

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,14 +1,37 @@
-#include "main.h"
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 /**
- * _strpbrk - function that searches a string for any of a set of bytes
+ * _strpbrk - function revers string
  * @s : input
  * @accept : input
  *
- * Return: return the rest of pointer,
+ * Return: the rest of string
  */
 char *_strpbrk(char *s, char *accept)
 {
+	int p = -1, i, j;
 
+	for (i = 0; *accept != '\0'; i++)
+	{
+		for (j = 0; *s != '\0'; j++)
+		{
+			if (*accept == *s)
+			{
+				if (p >= j)
+					p = j;
+				if (p == -1)
+					p = j;
+				break;
+			}
+			else
+				s++;
+		}
+		accept++;
+		s -= j;
+	}
+	if (p == -1)
+		return ('\0');
+	else
+		return (s += p);
 }
