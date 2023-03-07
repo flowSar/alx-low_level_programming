fe98307cf5c73f5ee6f166ee67ea975cd6130b2f
flowsar <brahim.sar151@gmail.com>
2023-03-07 19:07:13 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _strstr - function revers string
+ * @haystack : input
+ * @needle : input
+ *
+ * Return: the rest of string after finding first char.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+			return (haystack + i);
+	}
+	return ('\0');
+}
