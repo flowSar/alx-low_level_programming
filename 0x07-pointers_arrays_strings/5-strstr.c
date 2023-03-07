6bcb6e3e66af3a145c72c268543e7bd7de0a17b0
flowsar <brahim.sar151@gmail.com>
2023-03-07 21:58:56 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -35,14 +35,3 @@ char *_strstr(char *haystack, char *needle)
 	else
 		return ('\0');
 }
-
-int main(void)
-{
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
-
-    t = _strstr(s, f);
-    printf("%s\n", t);
-    return (0);
-}
