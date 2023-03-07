8a716bc1b2875b39f57d36728788a2cb5233beed
flowsar <brahim.sar151@gmail.com>
2023-03-07 21:48:38 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,22 +1,37 @@
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 /**
- * _strstr - function revers string
+ * _strstr - function that locates a substring.
  * @haystack : input
  * @needle : input
  *
- * Return: the rest of string after finding first char.
+ * Return: the substing you alocate
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	int i, j, p = -1;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
-			return (haystack + i);
+			p = i;
+			break;
 		}
 	}
-	return ('\0');
+	if (p != -1)
+	{
+		i = 0;
+		for (j = p; needle[j] != '\0'; j++)
+		{
+			if (haystack[j] == needle[i])
+				i++;
+			else
+				return ('\0');
+		}
+		return (haystack + p);
+	}
+	else
+		return ('\0')
 }
