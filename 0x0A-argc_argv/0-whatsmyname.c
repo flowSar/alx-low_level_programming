2ca51e5ca73dd3dc8789c21ce6586bef4f43ade5
flowsar <brahim.sar151@gmail.com>
2023-03-10 18:52:06 +0100

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry function
+ * @argc : input
+ * @argv : input
+ *
+ * Return: o if program success.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
