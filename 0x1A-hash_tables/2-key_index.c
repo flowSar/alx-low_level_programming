0debba43620d15487f82aca05a26ec779dc7a1f1
flowsar <brahim.sar151@gmail.com>
2024-04-16 23:54:45 +0100

    key_inde

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,18 @@
 #include "hash_tables.h"
+
+/**
+ * key_index - this function return the index of each key
+ * @key: key
+ * @size: size of of array of hashtable
+ *
+ * Return: return the index of the key on hashtable array
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
