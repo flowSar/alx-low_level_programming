20e13e80faefcd18d46bd5a05d973d975bafe023
flowsar <brahim.sar151@gmail.com>
2024-04-18 00:00:00 +0100

    retrieve from hash table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,26 @@
 #include "hash_tables.h"
+/**
+ * hash_table_get - this function retreive valur from hashtable
+ * base on the kek_>index
+ * @ht: hash table reference
+ * @key: key elment
+ *
+ * Return: Return the found value or NULL if it isn't found.
+ */
+
+
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+		return (NULL);
+
+	return (ht->array[index]->value);
 }
