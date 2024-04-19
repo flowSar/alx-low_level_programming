e999fc9fb262ecca34c3e2895825d14cf4c1825d
flowsar <brahim.sar151@gmail.com>
2024-04-19 01:29:55 +0100

    I'm not in US

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,12 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
+	int number = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int number = atoi(argv[1]);
+	number = atoi(argv[1]);
 	if (number < 0)
 		printf("0\n");
 	return(0);
