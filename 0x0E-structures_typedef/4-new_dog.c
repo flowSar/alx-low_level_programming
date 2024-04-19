c2369700d28316e96dd7d878dc9f55a6679e9b82
flowsar <brahim.sar151@gmail.com>
2024-04-19 14:42:24 +0100

    allocate memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,6 +17,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
+	if (new_dog == NULL)
+		return (NULL);
+
 	new_dog->name = malloc(get_length(name) + 1);
 	new_dog->owner = malloc(get_length(owner) + 1);
 
