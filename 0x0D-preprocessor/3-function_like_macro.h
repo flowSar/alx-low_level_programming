70e8c401807c0ded0b2ef928ec5dab03ac187828
flowsar <brahim.sar151@gmail.com>
2024-04-20 00:19:41 +0100

    abs function as a macro argument

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABS_TEST
+#define ABS_TEST
+
+#define ABS(x) ((x) < (-1) ? (-1) * (x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+
+#define TEST_ABS
+#ifndef TEST_ABS
+
+#define ABD(x) (x) * (x)
+
+
+#endif
