c28bd715cb74604e63b595e664293f1096e31e6f
flowsar <brahim.sar151@gmail.com>
2024-07-17 10:34:34 +0100

    binary sreach

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,66 @@
+#include "search_algos.h"
+/**
+ * binary_search - binary search function
+ *
+ * @array: array we're looking at
+ * @size: size of the arrat
+ * @value: value that we're looking for in the array
+ *
+ * Return: return -1 if the value not found or value index if was found.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	if (array == NULL)
+		return (-1);
+	index = binary_search_handler(array, value, 0, size);
+
+	return (index);
+}
+/**
+ * binary_search_handler - this function use divide and conqure algorithm
+ * to search for a value in the array
+ *
+ * @array: array we're looking at
+ * @value: value that we're looking for in the array
+ * @f: first index that we're going to start searching from
+ * @l: last index that we're going to stop searching at
+ *
+ * Return: return -1 if the value not found or value index if was found.
+ */
+int binary_search_handler(int *array, int value,  int f, int l)
+{
+	int middle = (f + l) / 2;
+
+	if (f > l || f == l)
+		return (-1);
+
+	searching_in_array(array, f, l);
+	if (value == array[middle])
+		return (middle);
+	if (array[middle] > value)
+		return (binary_search_handler(array, value, f, middle));
+	else
+		return (binary_search_handler(array, value, middle + 1, l));
+}
+/**
+ * searching_in_array - the array that we're searching at
+ *
+ * @array: array we're looking at
+ * @f: first index of th e array
+ * @l: last index the array
+ */
+void searching_in_array(int *array, int f, int l)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = f; i < l; i++)
+	{
+		printf("%i", array[i]);
+		if (i < l - 1)
+			printf(",");
+	}
+	printf("\n");
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,11 @@
-#ifndef ALGO
-#define ALGO
+#ifndef ALGO_TEST
+#define ALGO_TEST
 #include <stdlib.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_handler(int *array, int value,  int f, int l);
+void searching_in_array(int *array, int f, int l);
 
 #endif
