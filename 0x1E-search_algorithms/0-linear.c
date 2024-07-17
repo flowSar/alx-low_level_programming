78c1fbfc7c6dd934f36fda418d5db6a37faabb8f
flowsar <brahim.sar151@gmail.com>
2024-07-17 09:36:18 +0100

    up

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,10 +11,10 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
-		return (-1);
 	size_t i;
 
+	if (array == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%li] = [%i]\n", i, array[i]);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,6 @@
 #ifndef ALGO
 #define ALGO
-
+#include <stdlib.h>
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
