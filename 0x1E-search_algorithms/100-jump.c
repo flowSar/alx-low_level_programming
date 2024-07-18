7f3ed8297cbb3dec96a5cdd368c85d4971be5a59
flowsar <brahim.sar151@gmail.com>
2024-07-18 13:17:47 +0100

    remove typo

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -34,7 +34,7 @@ int jump_search(int *array, size_t size, int value)
 			end += jump_steps;
 			if (end >= (int)size)
 			{
-				printf("hValue checked array[%i] = [%i]\n", i, array[i]);
+				printf("Value checked array[%i] = [%i]\n", i, array[i]);
 				printf("Value found between indexes [%i] and [%i]\n", start, end);
 				return (compare(array, i, size - 1, value));
 			}
